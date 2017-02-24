d956d615eb108f9a694d9488073fb481eba70101
arunan <arunans23@gmail.com>
2017-02-24 20:17:27 +0530

    add navigation to navbar items

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import {RouterModule} from "@angular/router";
+import {RecipesComponent} from "./recipes/recipes.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+
+
+const APP_ROUTES = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'shopping-list', component: ShoppingListComponent}
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
