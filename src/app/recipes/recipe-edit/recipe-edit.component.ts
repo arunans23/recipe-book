99ba8cac5e357317a9194672c7fb64555cdb2c19
arunan <arunans23@gmail.com>
2017-02-28 13:18:25 +0530

    add recipe form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RecipeService} from "../recipe.service";
+import {Subscription} from "rxjs";
+import {Recipe} from "../recipe";
+import {FormArray, FormGroup, FormControl, Validators, FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy{
+  private subscription: Subscription;
+  private recipeIndex: number;
+  private recipe: Recipe;
+  private isNew = true;
+  recipeForm: FormGroup;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private sls: RecipeService, private formBuilder: FormBuilder) {
+
+  }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+    (params: any) => {
+      if (params.hasOwnProperty('id')){
+        this.isNew = false;
+        this.recipeIndex = +params['id'];
+        this.recipe = this.sls.getRecipe(this.recipeIndex);
+      } else {
+        this.isNew = true;
+        this.recipe = null;
+      }
+      console.log(this.isNew);
+    }
+
+    );
+  }
+
+  private initForm(isNew: boolean){
+    let recipeName = '';
+    let recipeImageUrl = '';
+    let recipeContent = '';
+    let recipeIngredients : FormArray = new FormArray([]);
+
+    if(!this.isNew){
+      for (let i = 0; i < this.recipe.ingredients.length; i++){
+        recipeIngredients.push(
+          new FormGroup({
+            name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+            amount: new FormControl(this.recipe.ingredients[i].amount, Validators.required, Validators.pattern("\\d+")),
+            }
+          )
+        )
+      }
+      recipeName = this.recipe.name;
+      recipeImageUrl = this.recipe.imagePath;
+      recipeContent = this.recipe.description;
+
+
+    }
+
+    this.recipeForm = this.formBuilder.group({
+      name: [recipeName, Validators.required],
+      imagePath: [recipeImageUrl, Validators.required],
+      description: [recipeContent, Validators.required],
+      ingredients: recipeIngredients
+    });
+
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
